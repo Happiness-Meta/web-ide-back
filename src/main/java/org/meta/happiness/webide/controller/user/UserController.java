932147074dad09caf41223325728d22a6f359c1c
changDDAO <younch8342@naver.com>
2024-02-12 11:56:43 +0900

    feat: UserController 작성하기

---
+++ src/main/java/org/meta/happiness/webide/controller/user/UserController.java
@@ -0,0 +1,73 @@
+package org.meta.happiness.webide.controller.user;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.meta.happiness.webide.dto.response.MultipleResult;
+import org.meta.happiness.webide.dto.response.SingleResult;
+import org.meta.happiness.webide.dto.user.*;
+import org.meta.happiness.webide.exception.EmailPatternException;
+import org.meta.happiness.webide.security.JwtUtil;
+import org.meta.happiness.webide.service.ResponseService;
+import org.meta.happiness.webide.service.UserService;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.meta.happiness.webide.dto.response.Result;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+@RequestMapping("/api")
+public class UserController {
+    private final UserService userService;
+    private final ResponseService responseService;
+    private final JwtUtil jwtUtil;
+
+    //회원가입
+    @PostMapping("/sign/register")
+    public SingleResult<UserResponseDto> register(@RequestBody @Validated UserRegisterDto form,
+                                                  BindingResult bindingResult) {
+        if (bindingResult.hasFieldErrors("email"))  // request 단에서 넘어온 양식을 validation으로 check하고,
+            //문제 있을 시 Exception을 던져줌 Exception은 ExceptionAdvice class에서 다루고 있음.
+            throw new EmailPatternException();
+        if(bindingResult.hasFieldErrors("password")) throw new IllegalArgumentException("password 양식에 안맞음");
+        return responseService.handleSingleResult(userService.registerUser(form));
+    }
+
+
+    //로그인
+    @PostMapping("/sign/login")
+    public SingleResult<UserLoginResponseDto> login(@RequestBody LoginFormDto form) {
+        return responseService.handleSingleResult(userService.loginUser(form));
+    }
+    //모든 회원 조회
+    @GetMapping("/users")
+    public MultipleResult<UserResponseDto> findAllUsers() {
+        return responseService.handleListResult(userService.findAllUser());
+    }
+    //단일 회원 조회
+    @GetMapping("/user/{id}")
+    public SingleResult<UserResponseDto> findUser(@PathVariable("id")Long userId) {
+        return responseService.handleSingleResult(userService.findUser(userId));
+    }
+    //회원 정보 수정
+    @PutMapping("/user/{id}")
+    public SingleResult<UserResponseDto> updateUser(@PathVariable("id") Long userId,
+                                                    @RequestBody UserUpdateDto request) {
+        return responseService.handleSingleResult(userService.updateUser(userId, request));
+    }
+    //회원 삭제
+    @DeleteMapping("/user/{id}")
+    public Result deleteUser(@PathVariable("id") Long userId) {
+        userService.deleteUser(userId);
+        return responseService.handleSuccessResult();
+    }
+    //토큰 재발급
+    @PostMapping("/refresh")
+    public SingleResult<UserLoginResponseDto> refreshToken(HttpServletRequest request) {
+        String headerAuth = request.getHeader("Authorization");
+        String auth = headerAuth.substring(7);
+        return responseService.handleSingleResult(userService.refreshToken(auth));
+    }
+}
